d4cad000484e8f22145809e26433092ba8fb53ae
Jean Tubay <jean.tubay.92@gmail.com>
2020-03-14 18:21:52 -0400

    set up state for pokemon component

--- src/components/pokemon/Pokemon.jsx
+++ src/components/pokemon/Pokemon.jsx
@@ -3,10 +3,28 @@ import axios from 'axios';
 
 
 export default class Pokemon extends Component {
+    state = {
+        name: '',
+        pokemonIndex: '',
+        imageUrl: ''
+    };
+
+    async componentDidMount() {
+        const {pokemonIndex} = this.props.match.params;
+
+        const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonIndex}/`;
+        const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${pokemonIndex}/`;
+
+        const pokemonRes = await axios.get(pokemonUrl);
+        const name = pokemonRes.data.name
+
+        this.setState({name})
+
+    }
     render(){
         return(
             <div>
-                <h1>hello</h1>
+                <h1>{this.state.name}</h1>
             </div>
         )
     }
