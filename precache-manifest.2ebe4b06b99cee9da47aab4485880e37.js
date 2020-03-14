6beffa1d51e94a2375f9132cce67493bc1d4d49b
Jean Tubay <jean.tubay.92@gmail.com>
2020-03-14 19:42:56 -0400

    Updates

---
+++ precache-manifest.2ebe4b06b99cee9da47aab4485880e37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d796af2a61456099c64ad648b1a88b4a",
+    "url": "/reactPokeDex/index.html"
+  },
+  {
+    "revision": "05216bc499298fded7ec",
+    "url": "/reactPokeDex/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "eaaf0eaef708366c143c",
+    "url": "/reactPokeDex/static/css/main.d290123a.chunk.css"
+  },
+  {
+    "revision": "05216bc499298fded7ec",
+    "url": "/reactPokeDex/static/js/2.240a5179.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/reactPokeDex/static/js/2.240a5179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaaf0eaef708366c143c",
+    "url": "/reactPokeDex/static/js/main.dc66addc.chunk.js"
+  },
+  {
+    "revision": "8373c62e4a934ea4535f",
+    "url": "/reactPokeDex/static/js/runtime-main.8640478a.js"
+  }
+]);
